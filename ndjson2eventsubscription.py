384b9062233a7f40a545d770e284f4b60299ef34
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-09-13 17:54:45 -0700

    Update ndjson2eventsubscription.py

--- ndjson2eventsubscription.py
+++ ndjson2eventsubscription.py
@@ -3,6 +3,18 @@
 import json
 import pprint
 
+"""
+    <QueryList>
+    <Query Id="0" Path="Microsoft-Windows-Sysmon/Operational">
+      <Select Path="Microsoft-Windows-Sysmon/Operational">
+      *[System[EventID='1']] and
+      *[EventData[Data[@Name='ParentImage'] != '']] and
+      *[EventData[Data[@Name='Image'] != '']]
+      </Select>
+    </Query>
+    </QueryList>
+"""
+
 with open("export.ndjson") as ndjson:
     json_objects = []
     for ndjson_object in ndjson:
@@ -11,7 +23,12 @@ with open("export.ndjson") as ndjson:
     kibana_filters = json_objects[1]["attributes"]["filters"]
     for kibana_filter in kibana_filters:
         field_name = kibana_filter["meta"]["key"]
+        if field_name == "process.parent.executable.keyword":
+            foo = "*[EventData[Data[@Name='ParentImage'] != '%s']] and" % (field_name)
+            print(foo)
+        else:
+            pprint.pprint(field_name)
+        
         values = kibana_filter["meta"]["params"]
-        pprint.pprint(field_name)
         pprint.pprint(values)
         print("\n")
